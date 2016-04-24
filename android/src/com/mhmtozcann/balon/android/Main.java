10639cde479e0a5c5038eac5163a66ec2a4af479
Mehmet Özcan <mehmetozcan007@gmail.com>
2016-04-24 18:52:44 +0300

    Initial commit

---
+++ android/src/com/mhmtozcann/balon/android/Main.java
@@ -0,0 +1,17 @@
+package com.mhmtozcann.balon.android;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+/**
+ * Created by razor on 23.04.2016.
+ */
+public class Main extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
